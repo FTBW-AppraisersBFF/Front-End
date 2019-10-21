047382ba1d8ba59f3d2143c0ac3a4862d604079b
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-21 15:23:39 +0100

    imported components for testing in App Container

--- appraisers-bff/src/App.js
+++ appraisers-bff/src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+
+import "./App.css";
+import PrivateRoute from "./privateroute";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path="/" component={Login} />
+      <Route path="/signup" component={Signup} />
+      <PrivateRoute exact path="/dashboard" component={Dashboard} />
     </div>
   );
 }
