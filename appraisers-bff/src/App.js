dc6701e74760eb4656c95377b2067bce946186d8
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-22 20:58:13 +0100

    imported navbar

--- appraisers-bff/src/App.js
+++ appraisers-bff/src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 
 import "./App.css";
 import PrivateRoute from "./privateroute";
+import Navigation from "./components/Navigation";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Dashboard from "./components/Dashboard";
@@ -11,8 +12,9 @@ import AccountForm from "./components/AccountForm";
 function App() {
   return (
     <div className="App">
+      <Route exact path="/login" component={Navigation} />
       <Route exact path="/login" component={Login} />
-      <Route path="/signup" component={Signup} />
+      <Route exact path="/signup" component={Signup} />
       <PrivateRoute exact path="/dashboard" component={Dashboard} />
       <PrivateRoute exact path="/account" component={AccountForm}/>
     </div>
