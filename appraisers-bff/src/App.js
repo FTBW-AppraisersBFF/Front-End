a8c2c74d5e885c4b0f37384444e47d933107b71e
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-23 18:41:31 +0100

    state brought to top

--- appraisers-bff/src/App.js
+++ appraisers-bff/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Route } from "react-router-dom";
 
 import "./App.css";
@@ -9,16 +9,28 @@ import Signup from "./components/Signup";
 import Dashboard from "./components/Dashboard";
 import AccountForm from "./components/AccountForm";
 import HouseData from "./components/HouseData";
+import EditHouse from "./components/EditHouse";
 
-function App() {
+function App(props) {
+  const [houseDetails, setHouseDetails] = useState([]);
+  const [houseList, setHouseList] = useState([]);
+  
   return (
     <div className="App">
       <Route  path="/" component={Navigation} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/signup" component={Signup} />
       <PrivateRoute exact path="/dashboard" component={Dashboard} />
-      <PrivateRoute exact path="/account" component={AccountForm}/>
-      <PrivateRoute exact path="/appraised" component={HouseData}/>
+      <PrivateRoute path="/edit/:id" component={EditHouse} props={{
+        houseList, setHouseList, houseDetails, setHouseDetails
+      }}/>
+      <PrivateRoute exact path="/account" component={AccountForm} props={{
+        houseList, setHouseList, houseDetails, setHouseDetails
+      }}/>
+      <PrivateRoute exact path="/appraised" component={HouseData} props={{ houseList, setHouseList
+        // accountList: /*thing from state*/
+        // setAccountList: /*thing from state*/
+      }}/>
     </div>
   );
 }
